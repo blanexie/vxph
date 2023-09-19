8350dafa3d10f3e83939a7860332f74d987a7b60
xiezhicheng <xiezhicheng@xiaomi.com>
2023-09-19 19:31:58 +0800

    开始加载插件

--- src/main/kotlin/com/github/blanexie/vxph/MainVerticle.kt
+++ src/main/kotlin/com/github/blanexie/vxph/MainVerticle.kt
@@ -9,10 +9,10 @@ import cn.hutool.setting.SettingUtil
 import com.github.blanexie.vxph.core.AbstractVerticle
 import com.github.blanexie.vxph.core.entity.Message
 import com.github.blanexie.vxph.plugin.outbund.JdbcPlugin
-import io.vertx.core.Vertx
+import io.vertx.kotlin.core.Vertx
 import kotlinx.coroutines.launch
 import org.slf4j.LoggerFactory
-
+import com.github.blanexie.vxph.utils.Status
 
 /**
  * 启动三件事
@@ -26,19 +26,40 @@ class MainVerticle : AbstractVerticle("mainVerticle", "0", "0") {
   private val setting = SettingUtil.get("vxph.properties")
   override suspend fun handleEnd() {
     loadJdbcPlugin()
+    loadPluginVerticle()
   }
 
   private suspend fun loadPluginVerticle() {
+    val receiver= getCoreJdbcPluginTopic();
+    val message = Message(topic, receiver)
+    val sql = "SELECT * FROM v_plugin WHERE status =1 "
+    message.data = mapOf("sql" to sql, "params" to mapOf<String, Any>())
+    sendMessage(message){
+        val result = it.data["result"] as List<*>
+        if (CollUtil.isEmpty(result)) {
+          log.warn("数据库插件表中未找到插件， 无插件加载，需确认是否正常.......")
+        } else {
+          //TODO 开始加载插件表
+          log.info("插件加载完成.......")
+        }
+      }
+    }
+  }
 
+  private fun getCoreJdbcPluginTopic():String{
+    val jdbcUrl = setting.getStr("vxph.database.jdbc.url")
+    val username = setting.getStr("vxph.database.username", "")
+    return "jdbcPlugin:$jdbcUrl:$username"
   }
 
   private fun loadJdbcPlugin() {
+    val receiver= getCoreJdbcPluginTopic();
+
     val jdbcUrl = setting.getStr("vxph.database.jdbc.url")
     val username = setting.getStr("vxph.database.username", "")
     val password = setting.getStr("vxph.database.password", "")
     val maxPoolSize = setting.getInt("vxph.database.maxPoolSize", 16)
     val jdbcPlugin = JdbcPlugin(jdbcUrl, username, password, maxPoolSize)
-    val receiver = "jdbcPlugin:$jdbcUrl:$username"
     vertx.deployVerticle(jdbcPlugin) {
       launch {
         if (it.succeeded()) {
