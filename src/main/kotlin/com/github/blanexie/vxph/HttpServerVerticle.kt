6a8b645bc78e962ad1905127c89c44592dc57c5f
xiezhicheng <xiezhicheng@xiaomi.com>
2023-10-13 17:47:07 +0800

    数据库和扫描加载Action的调整

--- src/main/kotlin/com/github/blanexie/vxph/HttpServerVerticle.kt
+++ src/main/kotlin/com/github/blanexie/vxph/HttpServerVerticle.kt
@@ -1,4 +1,4 @@
-package com.github.blanexie.vxph.tracker
+package com.github.blanexie.vxph
 
 import com.github.blanexie.vxph.tracker.http.RouterLoadFactory
 import com.github.blanexie.vxph.utils.setting
@@ -7,15 +7,15 @@ import io.vertx.ext.web.Router
 import io.vertx.kotlin.coroutines.CoroutineVerticle
 import org.slf4j.LoggerFactory
 
-class HttpServerVerticle(val port: Int) : CoroutineVerticle() {
+class HttpServerVerticle : CoroutineVerticle() {
 
     private val log = LoggerFactory.getLogger(this::class.java)
-    private val actionPackage = setting.getStr("vxph.http.action.package")
-
+    private val port = setting.getInt("vxph.http.server.port", 8061)
     override suspend fun start() {
+
         val httpServer = vertx.createHttpServer()
         val router = Router.router(vertx)
-        val routerLoadFactory = RouterLoadFactory(actionPackage)
+        val routerLoadFactory = RouterLoadFactory(this::class.java.packageName)
         routerLoadFactory.loadPathRouter(router)
         httpServer.requestHandler(router).listen(port)
         log.info("开始http服务， 端口：{}", port)
@@ -25,7 +25,7 @@ class HttpServerVerticle(val port: Int) : CoroutineVerticle() {
 
 
 fun main() {
-    val httpServerVerticle = HttpServerVerticle(8086)
+    val httpServerVerticle = HttpServerVerticle()
     val vertx = Vertx.vertx()
     vertx.deployVerticle(httpServerVerticle)
 }
--- src/main/resources/vxph-ddl.sql
+++ src/main/resources/vxph-ddl.sql
@@ -15,16 +15,16 @@ create table IF NOT EXISTS Peer
     constraint peer_pk primary key (infoHash, passkey, peerId)
 );
 
-create index peer_updateTime_index
+create index if not exists peer_updateTime_index
     on Peer (updateTime);
 
-create index peer_createTime_index
+create index if not exists peer_createTime_index
     on Peer (createTime);
 
-create index peer_passkey_index
+create index if not exists peer_passkey_index
     on Peer (passkey);
 
-create index peer_peerId_index
+create index if not exists peer_peerId_index
     on Peer (peerId);
 
 ------------------------------------------------------------
@@ -42,16 +42,16 @@ create table IF NOT EXISTS UserTorrent
     constraint UserTorrent_pk primary key (passkey, infoHash)
 );
 
-create index UserTorrent_updateTime_index
+create index if not exists UserTorrent_updateTime_index
     on UserTorrent (updateTime);
 
-create index UserTorrent_createTime_index
+create index if not exists UserTorrent_createTime_index
     on UserTorrent (createTime);
 
-create index UserTorrent_infoHash_index
+create index if not exists UserTorrent_infoHash_index
     on UserTorrent (infoHash);
 
-create index UserTorrent_userId_index
+create index if not exists UserTorrent_userId_index
     on UserTorrent (userId);
 
 ------------------------------------------------------------
@@ -72,16 +72,16 @@ create table IF NOT EXISTS User
     updateTime TEXT              not null
 );
 
-create unique index User_name_index
+create unique index if not exists User_name_index
     on User (name);
 
-create unique index User_email_index
+create unique index if not exists User_email_index
     on User (email);
 
-create index User_updateTime_index
+create index if not exists User_updateTime_index
     on User (updateTime);
 
-create index User_createTime_index
+create index if not exists User_createTime_index
     on User (createTime);
 
 
@@ -103,16 +103,16 @@ create table IF NOT EXISTS Invite
     updateTime TEXT              not null
 );
 
-create unique index Invite_name_index
+create unique index if not exists Invite_name_index
     on Invite (sender);
 
-create unique index Invite_email_index
+create unique index if not exists Invite_email_index
     on Invite (email);
 
-create index Invite_updateTime_index
+create index if not exists Invite_updateTime_index
     on Invite (updateTime);
 
-create index Invite_createTime_index
+create index if not exists Invite_createTime_index
     on Invite (createTime);
 
 
@@ -135,13 +135,13 @@ create table IF NOT EXISTS Account
     updateTime  TEXT                     not null
 );
 
-create unique index Account_userId_index
+create unique index if not exists Account_userId_index
     on Account (userId);
 
-create index Account_updateTime_index
+create index if not exists Account_updateTime_index
     on Account (updateTime);
 
-create index Account_createTime_index
+create index if not exists Account_createTime_index
     on Account (createTime);
 
 ------------------------------------------------------------
@@ -165,10 +165,10 @@ create table IF NOT EXISTS Torrent
     updateTime   TEXT              not null
 );
 
-create index Torrent_updateTime_index
+create index if not exists Torrent_updateTime_index
     on Torrent (updateTime);
 
-create index Torrent_createTime_index
+create index if not exists Torrent_createTime_index
     on Torrent (createTime);
 
 ------------------------------------------------------------
